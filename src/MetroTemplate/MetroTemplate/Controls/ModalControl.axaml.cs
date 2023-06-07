a5da0049133f65d50dae62e1e7e9a5c250aa217a
balbarak <balbarak@live.com>
2023-06-07 18:18:27 +0300

    modal now work

--- src/MetroTemplate/MetroTemplate/Controls/ModalControl.axaml.cs
+++ src/MetroTemplate/MetroTemplate/Controls/ModalControl.axaml.cs
@@ -1,8 +1,10 @@
 using Avalonia;
+using Avalonia.Animation;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Media;
 using Avalonia.Metadata;
+using System;
 
 namespace MetroTemplate.Controls
 {
@@ -14,9 +16,22 @@ namespace MetroTemplate.Controls
         public static readonly StyledProperty<bool> IsOpenProperty =
            AvaloniaProperty.Register<ModalControl, bool>(nameof(IsOpen), defaultValue: false);
 
+        public static readonly StyledProperty<TimeSpan> SpeedProperty =
+           AvaloniaProperty.Register<ModalControl, TimeSpan>(nameof(Speed), defaultValue: TimeSpan.FromSeconds(.3));
+
+
         [Content]
         public object Content { get => GetValue(ContentProperty); set => SetValue(ContentProperty, value); }
 
         public bool IsOpen { get => GetValue(IsOpenProperty); set => SetValue(IsOpenProperty, value); }
+
+        public TimeSpan Speed { get => GetValue(SpeedProperty); set => SetValue(SpeedProperty, value); }
+
+
+        public ModalControl()
+        {
+            
+        }
+
     }
 }
