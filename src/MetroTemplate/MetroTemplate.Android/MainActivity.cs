c85e512d7c0a396cef9a411d502593c7dff4cd4b
balbarak <balbarak@live.com>
2023-08-09 22:15:15 +0300

    update !

--- src/MetroTemplate/MetroTemplate.Android/MainActivity.cs
+++ src/MetroTemplate/MetroTemplate.Android/MainActivity.cs
@@ -1,11 +1,23 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Avalonia;
 using Avalonia.Android;
+
 
 namespace MetroTemplate.Android
 {
-    [Activity(Label = "MetroTemplate.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
-    public class MainActivity : AvaloniaMainActivity
+
+    [Activity(
+        Label = "MetroTemplate.Android",
+        Theme = "@style/MyTheme.NoActionBar",
+        Icon = "@drawable/icon",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+    public class MainActivity : AvaloniaMainActivity<App>
     {
+        protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+        {
+            return base.CustomizeAppBuilder(builder);
+        }
     }
 }
